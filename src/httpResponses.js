a6540531c9d5cfc8e17a3fcc69ec1b6279f373c3
Juan Delgadillo <j.mnuelh@gmail.com>
2020-07-13 20:53:08 +0200

    feat: add new badRequest, unauthorized and serviceUnavailable httpResponse helpers

--- src/httpResponses.js
+++ src/httpResponses.js
@@ -1,3 +1,17 @@
+const { httpErrorCodes } = require('./consts');
+
 module.exports.ok = function(res, payload) {
-  return res.status(200).json({ message: 'ok', ...payload });
+  return res.status(httpErrorCodes.ok).json({ ...payload });
+};
+
+module.exports.badRequest = function(res, payload) {
+  return res.status(httpErrorCodes.badRequest).json({ message: 'Bad request', ...payload });
+};
+
+module.exports.unauthorized = function(res, payload) {
+  return res.status(httpErrorCodes.unauthorized).json({ message: 'Unauthorized', ...payload });
+};
+
+module.exports.serviceUnavailable = function(res, payload) {
+  return res.status(httpErrorCodes.serviceUnavailable).json({ message: 'Service Unavailable', ...payload });
 };
