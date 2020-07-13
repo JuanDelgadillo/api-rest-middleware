4d5e05ac53bc0cdf355c6365fecc70ef3cc2b506
Juan Delgadillo <j.mnuelh@gmail.com>
2020-07-13 20:55:22 +0200

    feat: add functionality to "authenticate" controller

--- src/auth/controller.js
+++ src/auth/controller.js
@@ -1,11 +1,50 @@
-const { ok } = require('../httpResponses');
+const { ok, badRequest, unauthorized, serviceUnavailable } = require('../httpResponses');
+const { insuranceApiEndpoint, httpErrorCodes } = require('../consts');
+const axios = require('axios');
 
 
 const authenticate = async (req, res) => {
   const { body = {} } = req;
   const { username = '', password = '' } = body;
 
-  return ok(res);
+  if (!username || !password) {
+    return badRequest(res, { 
+      message: 'body should have mandatory properties "username" and "password"',
+      code: httpErrorCodes.badRequest,
+    });
+  }
+
+  try {
+    const response = await axios.post(`${insuranceApiEndpoint}/api/login`, {
+      client_id: username,
+      client_secret: password,
+    });
+
+    const { token = '', type = '' } = response.data;
+    const expires_in = JSON.parse(
+      Buffer.from(
+        token.split('.')[1],
+        'base64'
+      ).toString()
+    ).exp;
+
+    return ok(res, {
+      token,
+      type,
+      expires_in,
+    });
+  } catch (error) {
+    const { data } = error.response;
+
+    if (data.statusCode === httpErrorCodes.unauthorized) {
+      return unauthorized(res, { 
+        message: 'invalid username or password',
+        code: data.statusCode 
+      });
+    }
+
+    return serviceUnavailable(res);
+  }
 };
 
 module.exports = {
