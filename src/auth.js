f12b94b79ebc291dfbe1b535d2e6b0bcd1d0fde4
Juan Delgadillo <j.mnuelh@gmail.com>
2020-07-13 22:18:02 +0200

    feat: add basic authentication middleware

---
+++ src/auth.js
@@ -0,0 +1,15 @@
+const { unauthorized } = require('./httpResponses');
+
+const authenticate = (req, res, next) => {
+  if (!(req.headers && req.headers.authorization)) {
+    return unauthorized(res, {
+      message: 'No authorization header was found',
+    });
+  }
+  
+  next();
+};
+
+module.exports = {
+  authenticate,
+};
