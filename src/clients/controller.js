e97d8e942ef25b0005141ad73c20fb0c9d92e763
Juan Delgadillo <j.mnuelh@gmail.com>
2020-07-15 13:16:05 +0200

    feat: add functionality to "getClientById" controller

--- src/clients/controller.js
+++ src/clients/controller.js
@@ -1,17 +1,30 @@
-const { ok } = require('../httpResponses');
-const clientService = require('./service');
+const { ok, notFound } = require('../httpResponses');
+const clientsService = require('./service');
 
 
 const getClients = async (req, res) => {
-  const clients = await clientService.getClients(req);
+  let clients = await clientsService.getClientsWithPolicies(req);
 
   return ok(res, clients);
 };
 
 const getClientById = async (req, res) => {
-  const { id } = req.params;
+  const { id = '' } = req.params;
 
-  return ok(res);
+  if (!id) {
+    return badRequest(res, { 
+      message: 'request should have mandatory parameter "id"'
+    });
+  }
+
+  const clients = await clientsService.getClientsWithPolicies(req);
+  const client = clients.filter((client) => client.id === id);
+
+  if (client.length === 0) {
+    return notFound(res);
+  }
+
+  return ok(res, client);
 };
 
 const getClientPoliciesByClientId = async (req, res) => {
